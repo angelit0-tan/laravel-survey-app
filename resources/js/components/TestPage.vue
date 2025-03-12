62da6a785d1897358c20a6ebdec19b6f957b18a9
dayofdawn <dawnrflores@gmail.com>
2025-03-13 02:18:23 +0800

    test

--- resources/js/components/TestPage.vue
+++ resources/js/components/TestPage.vue
@@ -1,8 +1,250 @@
 <template>
-    <div>
-        Test Page 1 abc
+    <div class="text-xl small-caps mb-5 font-bold">
+        +  Meinungsumfrage Hulst- Vsn 3d
+    </div>
+    <div class="bg-gray-300 border border-black p-2 text-xl">
+        <p class="font-bold small-caps">            
+            Guten Tag.
+        </p>
+    </div>
+    <div class="border border-black p-2 mb-5">
+        <p class="font-bold my-5 text-lg">
+            Hier eine kurze Meinungsumfrage; sie richtet sich an die Bewohner von Hulst und Umgebung:
+        </p>
+
+        <p class="font-bold small-caps text-lg">Worum geht es?</p>
+        <p class="mb-5">            
+            Die Hulster Stadtverwaltung und ihre Bürgermeisterin. – Ihre Meinung zählt!
+        </p>
+
+        <p class="font-bold small-caps text-lg">Warum sollten Sie teilnehmen?</p>
+        <p class="mb-5">            
+            Unter allen Teilnehmern werden zehn Direktgewinne zu je 100 Euro verlost – für nur fünf Minuten Ihrer Zeit. 
+        </p>
+
+        <p class="font-bold small-caps text-lg">Wie läuft es ab?</p>
+        <p class="mb-5">            
+            Sie lesen das unten stehende Szenario und beantworten sechs kurze Fragen. – Sie müssen nur ankreuzen.
+            Daten zu Ihrer Person werden nicht gespeichert und nicht veröffentlicht.
+        </p>
+    </div>
+    <div class="border border-black p-2 mb-5">
+        <p class="font-bold small-caps mb-5 text-lg">Szenario:</p>
+
+        <p class="font-bold mb-5">Jetzt lesen Sie eine unglaubliche, aber wahre Geschichte:</p>
+        
+        <p>
+            Stellen Sie sich vor, Sie kaufen in Hulst ein Haus im Wert von 300.000 Euro. Das Haus steht unter Denkmalschutz. Kurz darauf verlangt die Stadt, dass Sie 32, ohne Ihr Mitspracherecht festgelegte "Instandhaltungs"-Maßnahmen an diesem Haus durchführen müssen.
+        </p>
+
+        <ul class="list-dash mb-5 pl-2">
+            <li>
+                Hierfür wird Ihnen eine Frist gesetzt.
+            </li>
+            <li>
+                Für den Fall, dass Sie diese Frist nicht einhalten, droht Ihnen die Stadt Hulst eine Strafe von 600.000 Euro an (das Doppelte dessen also, was Ihr Haus lt. Kaufpreis wert ist).
+            </li>
+            <li>
+                Falls Sie bei der Fertigstellung dieser 32 Punkte nur einen einzigen Punkt nicht korrekt ausführen 	(z.B. nicht identischer Farbton bei den Mauerfugen), dann wird ebenfalls die gesamte  Strafgebühr von 	600.000 für Sie fällig.
+            </li>
+        </ul>
+
+        <p class="mb-5">            
+            Sie beauftragen einen erfahrenen Bauunternehmer. Dieser erstellt einen Bauplan und entdeckt, dass das Haus einsturzgefährdet ist.<br />
+            Mehrere hochwertige Sachverständigen-Gutachten (sogar das von der Stadt Hulst selbst in Auftrag gegebene Gutachten) bestätigen einhellig diesen Sachverhalt und erklären dies Haus sogar für "abreißbar".<br />
+            Falls man dennoch versucht, das Haus zu retten, benötigt es vorab ein neues Fundament, um überhaupt gefahrlos im Haus arbeiten zu können.<br />
+            Allein solch ein neues Fundament kostet Sie zusätzlich um die 530.000 Euro – fast doppelt so viel wie der Kaufpreis des Hauses.<br />
+            Erst danach können Sie die 32 Arbeiten, also die von der Stadt Hulst geforderten Punkte, beginnen.<br />
+            Für diese 32 Arbeiten fallen nach Schätzung sämtlicher Experten für Sie minimal Kosten in Höhe weiterer 1.800.000 Euro an.
+        </p>
+
+        <p>            
+            Damit wird für Sie dreierlei überdeutlich:
+        </p>
+        <ul class="list-dash mb-5 pl-2">
+            <li>
+                <span class="small-caps">Erstens:</span> Die "32 Punkte"-Forderung der Stadt Hulst ist ohne eine umfassende Baumaßnahme   nach Meinung aller Experten –  <span class="italic">überhaupt nicht realisierbar</span>.
+            </li>
+            <li>
+                <span class="small-caps">Zweitens:</span> Die Gesamtheit dieser Baumaßnahme, die zwangsläufig erforderlich wäre, um die von der 	Stadt Hulst verlangten "32 Punkte" erfüllen zu können, erfordert von Ihnen einen Kostenaufwand von 	minimal 2.500.000 Euro und bewirkt für Sie einen Verlust von minimal 2,2 Millionen Euro (mehr als 	den 7-fachen Kaufpreis Ihres Hauses).
+            </li>
+            <li>
+                <span class="small-caps">Drittens:</span> Bei dieser Baumaßnahme handelt es sich schon längst nicht mehr um reine "Instandhaltungs"- Maßnahmen, sondern um umfassende "Restaurierungs"-Maßnahmen. – Genau solche "Restaurierungs"- Maßnahmen werden von der Denkmalschutzbehörde aber ausgeschlossen; sie fordert von den Eigentümern denkmalgeschützter Gebäude lediglich jährliche "Instandhaltungs"-Maßnahmen.
+            </li>
+        </ul>
+
+        <p class="mb-5">            
+            Sie, der Eigentümer dieses für 300.000 Euro gekauften Hauses, wenden sich deshalb jetzt schriftlich an die Stadtverwaltung Hulst und an die dortige Bürgermeisterin, Frau Jense-van Haarst. Sie bitten sie um ein Gespräch mit Ihnen bezüglich dieser Immobilien-Situation.<br />
+            Sie wissen, dass die Bürgermeisterin zu diesem Gespräch mit Ihnen verpflichtet ist. Dennoch beantwortet Frau Jense-van Haarst Ihre Briefe nicht. Sie antwortet zunächst nicht mal auf die Briefe Ihres Anwalts.<br />
+            Stattdessen fordert jetzt die Stadtverwaltung wegen "Fristüberschreitung" zur Erledigung der "32 Punkte" von Ihnen die Strafgebühr von 600.000 Euro. Sie zwingt Sie mit Drohung, einen Gerichtsvollzieher einzuschalten und Ihre Bankkonten zu pfänden, jetzt die 600.000 Euro "Strafgebühr" wegen Überschreitung des Fertigstellungs-Termins der "32 Punkte" auf das Konto der Stadtkasse Hulst zu zahlen.
+        </p>
+
+        <p class="mb-5">            
+            Auch anschließend erhalten Sie zunächst kein Antwortschreiben, geschweige denn einen Gesprächstermin, von Frau Jense-van Haarst, Bürgermeisterin der Stadt Hulst. Erst auf massives, mehrfaches schriftliches Drängen des von Ihnen eingeschaltete Rechtsanwalts nennt das Büro der Bürgermeisterin Jense-van Haarst am letzten Tag der von Ihnen gesetzten Antwortfrist 2 Tage als Terminvorschlag für ein Gespräch. (Dies Gespräch steht zur Zeit noch aus.)
+        </p>
+        
+        <p class="font-bold mb-5">
+            Soweit unsere Geschichte. Sie ist tatsächlich so passiert. Uns interessiert dazu Ihre Meinung: 
+        </p>
+
+        <p class="font-bold mb-5 pl-5">
+            Wie beurteilen SIE, als Bürger/in von Hulst und Umgebung, das Verhalten der Stadtverwaltung
+            und von Frau Jense-van Haarst, der Bürgermeisterin von Hulst?      
+        </p>
+
+        <p class="font-bold mb-5 pl-5">            
+            Und: Was würden SIE tun, wenn die Stadt Hulst oder die Bürgermeisterin mit Ihnen als Bürger/in 	auf diese Weise umgehen würde?
+        </p>
+
+        <p class="font-bold mb-5">            
+            Danke für Ihre Zeit und für's Mitdenken! Wir schätzen das wirklich sehr!
+        </p>
+
+        <p class="font-bold mb-5">            
+            Für Ihre Antwort machen Sie bitte einfach 6 Kreuzchen bei den unten stehen kurzen Fragen:
+        </p>
+    </div>
+
+    <div class="font-bold mb-5 small-caps text-xl">
+        Bitte blättern Sie um!
+    </div>
+
+    <div class="font-bold mb-5 small-caps text-xl">        
+        Hier die 6 Fragen:
+    </div>
+
+    <div class="grid grid-cols-2 mb-5 border border-black" v-for="question in questions" :key="question.id">
+        <div class="border-r border-black p-2 row-span-4">
+            {{ question.id}}.) <span v-html="question.question" />
+        </div>
+        <div class="">
+            <div class="flex justify-between" :class="{'border-t border-black' : index != 0}" v-for="(choice, index) in question.choices" :key="choice.id">
+                <div class="p-1.5">
+                    <input 
+                    type="checkbox" 
+                    :id="`question-${question.id}-choice-${index}`" 
+                    v-model="selectedAnswers[question.id][index]"
+                    :value="choice"
+                    />
+                    <label class="ml-2" :for="`question-${question.id}-choice-${index}`">
+                        <span v-html="choice" />
+                    </label>
+                </div>
+                <div class="border-l border-black w-20 text-center p-1.5" v-if=question.scores>
+                    {{ question.scores[index] }}
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="border-r border-l border-t border-black py-3 mt-16">
+
+    </div>
+    <div class="grid grid-cols-2 border border-black">
+        <div class="border-r border-black p-2">
+            <p class="font-bold">
+                Das war's schon.
+            </p>
+            <p class="mb-5">                
+                Wir danken Ihnen für Ihre wertvolle Meinung und Ihre Zeit!<br />
+                Gerne würden wir Sie dafür belohnen.
+            </p>
+            <p class="mb-5">                
+                Sie möchten an der Verlosung für die zehn 100 Euro Direktgewinne teilnehmen?<br />
+                Geben Sie hier Ihre E-Mail-Adresse an, so dass wir Sie im Gewinnfall benachrichtigen könnnen.
+            </p>
+            <p class="font-bold">                
+                Und nochmal: Wir speichern nach Abschluß dieser Arbeit keinerlei Daten von Ihnen! – Versprochen!
+            </p>
+        </div>
+        <div class="self-center p-2">
+            hre E-Mail-Adresse: <input class="ml-2 border-b border-rose-400" type="text" />
+        </div>
     </div>
 </template>
 <script setup>
+import { ref } from 'vue';
+
+const questions = [
+    {
+        id: 1,
+        question: 'Wie bewerten Sie die <span class="font-bold">"Verhältnismäßigkeit"</span> zwischen dem Kaufwert eines Hauses in Höhe 300.000 Euro und hierfür anschließend verlangten "Instandhaltungs"-Kosten in Höhe von 2,2 Millionen Euro?',
+        choices: [
+            'extrem <span class="italic">un</span>-verhältnismäßig',
+            'stark <span class="italic">un</span>-verhältnismäßig',
+            'nicht verhältnismäßig',
+            'im ausgewogenen Verhältnis.'
+        ],
+        scores: [
+            '-2',
+            '-1',
+            '+1',
+            '+2'
+        ],
+    },
+    {
+        id: 2,
+        question: 'Wenn bei einem Haus im Kaufwert von 300.000 Euro Baumaßnahmen von <span class="font-bold">2,2 Millionen Euro</span> notwendig werden, handelt es sich dann entweder nur um Kosten für "Instandhaltung", oder aber für "Restaurierung"?',
+        choices: [
+            'eindeutig "Restaurierungs"-Kosten',
+            'enorm hohe, aber nicht klar definierbare Kosten',
+            'weiß nicht',
+            'eindeutig nur "Instandhaltungs"-Kosten'
+        ],
+    },
+    {
+        id: 3,
+        question: 'Wie finden Sie das Verhalten der Stadtverwaltung Hulst, einen Bürger - zwecks "Instandhaltung" seines denkmalgeschützten Hauses – mit einer <span class="font-bold">Strafe von 600.000 Euro</span> zu bedrohen?',
+        choices: [
+            'überhaupt nicht gerechtfertigt',
+            'nicht gerechtfertigt',
+            'gerechtfertigt',
+            'sehr gerechtfertigt'
+        ],
+        scores: [
+            '-2',
+            '-1',
+            '+1',
+            '+2'
+        ],
+    },
+    {
+        id: 4,
+        question: 'Wie finden Sie es, wenn die <span class="font-bold">Bürgermeisterin</span> von Hulst schriftliche, mehrfach wiederholte Anfragen eines Bürgers nicht einmal beantwortet?',
+        choices: [
+            'sehr inakzeptabel',
+            'inakzeptabel',
+            'akzeptabel',
+            'völlig akzeptabel'
+        ],
+        scores: [
+            '-2',
+            '-1',
+            '+1',
+            '+2'
+        ],
+    },
+    {
+        id: 5,
+        question: 'Was würden Sie einem Bürger von Hulst <span class="font-bold">empfehlen</span>, von dem die Stadtverwaltung / die Bürgermeisterin für die (faktisch unmögliche) Nicht-Einhaltung eines Instandhaltungs-Termins eine Strafe von 600.000 Euro kassiert?',
+        choices: [
+            'Strafanzeige wegen Nötigung/Erpressung',
+            'Öffentlicher Protest',
+            'Umziehen in einer andere Stadt',
+            'Nix. So kommt doch Geld in die Stadtkasse.'
+        ],
+    },
+    {
+        id: 6,
+        question: 'Was würden <span class="font-bold">Sie persönlich</span> wohl am liebsten tun, wenn Sie in dieser Situation wären?<br />(Sie können hier <span class="italic">mehrere</span> Antworten ankreuzen.)',
+        choices: [
+            'Ich würde nichts tun.',
+            'Ich würde rechtliche Schritte einleiten.',
+            'Ich würde politisch aktiv werden.',
+            'Ich würde öffentlich protestieren.'
+        ],
+    },
+];
+
+const selectedAnswers = ref({});
 
 </script>
