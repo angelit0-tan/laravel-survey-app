d8ae1155baa9721e8c45326368f27e53657d09f0
dayofdawn <dawnrflores@gmail.com>
2025-03-13 18:26:14 +0800

    test

--- resources/js/components/MyHeader.vue
+++ resources/js/components/MyHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="flex justify-between" 
+        <div class="flex justify-between mb-5" 
            >
             <div 
                 class="">
@@ -10,6 +10,9 @@
                 >
                 <nav-menu></nav-menu>
             </div>
+        </div>
+        <div>
+          <img src="/images/header-bg.jpg" />
         </div>
     </div>
 </template>
