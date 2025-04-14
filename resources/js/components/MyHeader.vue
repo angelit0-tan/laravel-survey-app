fde6c81ca44c2047b3f270dc724599548b8a3441
dayofdawn <dawnrflores@gmail.com>
2025-04-14 19:10:45 +0800

    test

--- resources/js/components/MyHeader.vue
+++ resources/js/components/MyHeader.vue
@@ -35,7 +35,7 @@
         </div>
         <div class="relative">
           <img src="/images/header-hulst-new.jpg" class="h-60 lg:h-auto object-cover w-screen sm:w-full "/>
-          <img src="/images/bubble-2.png" class="bubble absolute w-1/2 xl:w-1/3"/>
+          <img src="/images/bubble-new-2.png" class="bubble absolute w-1/2 xl:w-1/3"/>
         </div>
         
     </div>
