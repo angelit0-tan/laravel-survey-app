fefbb488f8850d5834b5d27029e395cbf3d7036a
dayofdawn <dawnrflores@gmail.com>
2025-04-14 17:21:46 +0800

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,6 @@ const app = createApp({});
 app.component('TestPage',
     defineAsyncComponent(() => import('./components/TestPage.vue'))
 );
-app.component('NavMenu',
-    defineAsyncComponent(() => import('./components/NavMenu.vue'))
-);
 app.component('MyHeader',
     defineAsyncComponent(() => import('./components/MyHeader.vue'))
 );
